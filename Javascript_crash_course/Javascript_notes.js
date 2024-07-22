60c9aa5f0ecf304b0c630ae299aa1edbc1243991
StevenForler <stevensjsf@gmail.com>
2024-07-21 21:54:02 -0700

    JavaScript lesson notes

---
+++ Javascript_crash_course/Javascript_notes.js
@@ -0,0 +1,9 @@
+// this is how you make a comment. it is a single line comment
+
+// this is how you get any information from JavaScript that you can use to help debug your code basically JS equivalent to print()
+console.log('hello world') 
+// Also the Console log is really picky about having a comment next to it so running the log won't produce anything
+console.log(4) 
+console.log(4 + 4) 
+// to add an apostrophe in a string you can use \' to help ignore it so it doesn't break the full string weirdly
+console.log('I don\'t like how javascript works') 
