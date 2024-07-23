2e15bde4c1c87f6b8a7f6bbb9305e6ce72402bab
StevenForler <stevensjsf@gmail.com>
2024-07-22 23:06:55 -0700

    progressed in JS lesson

--- Javascript_crash_course/Javascript_notes.js
+++ Javascript_crash_course/Javascript_notes.js
@@ -8,3 +8,20 @@ console.log(4 + 4)
 // to add an apostrophe in a string you can use \' to help ignore it so it doesn't break the full string weirdly
 console.log('I don\'t like how javascript works') 
 // values are what data type something is. for example. strings are just sentences and integer are just whole numbers and there are more like that
+
+Variables = "pretty straight forward"
+// you're basically creating the equivalent of bucket that you will reuse to make your code much cleaner to process. can be used with any data type. but you bu
+// 3notvariable = "can\'t use a number in the beginning or spaces in your variable"
+// the first time you use a new variable you must initialize them.
+
+var // this one isn't used much at all anymore but just know it is a thing 
+
+let, explanation = "this is for when you know it will change and you'll let it change" // I don't understand why when not pre instantiated like below that this requires a comma and when pre-instantiated like below that it doesn't?
+sentenceTWO = "like this sentence will be the new value for this variable since the initialization for let will 'let it change' "
+const sentence = " you will use this if you know this variable will not ever change"
+
+// you can also do this way of initializing your variables as well and don't need the declarations
+let secondVariable
+let firstVariable, thirdVariable
+
+// assignment by reference
