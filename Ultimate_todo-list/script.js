51fe162329644f5b4b2ce572eca6d6d64472526e
StevenForler <stevensjsf@gmail.com>
2024-08-01 10:54:30 -0700

    Completed todo function

--- Ultimate_todo-list/script.js
+++ Ultimate_todo-list/script.js
@@ -12,10 +12,27 @@ function addTodo(event) {
     event.preventDefault();
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo")
-    const newTodo = document.createElement("li"):
+    const newTodo = document.createElement("li");
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
     // local storage function
+    saveLocalTodos(todoInput.value);
+    //completed button
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = '<i class="fas fa-check-circle"></li>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></li>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
 
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    
 }
