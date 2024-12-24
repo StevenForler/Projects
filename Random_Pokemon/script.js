b06abf39dabd6f097dd3c6c03dadcba1f7721b03
StevenForler <stevensjsf@gmail.com>
2024-12-23 16:03:14 -0800

    code examples to try with Set

--- Random_Pokemon/script.js
+++ Random_Pokemon/script.js
@@ -1,16 +1,23 @@
-// let pkmnId;
+let pkmnId;
 
-// document.getElementById("rollPkmn").onclick = function(){
-//     pkmnId = Math.floor(Math.random() * 1025) +1;
+document.getElementById("rollPkmn").onclick = function(){
+    pkmnId = Math.floor(Math.random() * 1025) +1;
 
-//     document.getElementById("pkmnNo").innerHTML = pkmnId;
-// }
-
-const uniqueNumbers = new Set(); 
-
-while (uniqueNumbers.size < 10000) { 
-  let randomNum = Math.floor(Math.random() * 10000) + 1; 
-  uniqueNumbers.add(randomNum); 
+    document.getElementById("pkmnNo").innerHTML = pkmnId;
 }
 
-console.log([uniqueNumbers]);
+// const uniqueNumbers = new Set(); 
+
+// while (uniqueNumbers.size < 10000) { 
+//   let randomNum = Math.floor(Math.random() * 10000) + 1; 
+//   uniqueNumbers.add(randomNum); 
+// }
+
+// console.log([uniqueNumbers]);
+
+
+// function getRandomKey(collection) {
+//     let keys = Array.from(collection.keys());
+//     return keys[Math.floor(Math.random() * keys.length)];
+// }
+// https://stackoverflow.com/questions/42739256/how-get-random-item-from-es6-map-or-set
