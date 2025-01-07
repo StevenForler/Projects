d6bbfcc5ffcbfbdaa7dd949938edc285ae7e7a73
StevenForler <stevensjsf@gmail.com>
2025-01-06 22:54:13 -0800

    updated set script

--- Random_Pokemon/script.js
+++ Random_Pokemon/script.js
@@ -6,11 +6,22 @@
 //     document.getElementById("pkmnNo").innerHTML = pkmnId;
 // }
 
-const uniqueNumbers = new Set(); 
 
-while (uniqueNumbers.size < 10000) { 
-  let randomNum = Math.floor(Math.random() * 10000) + 1; 
-  uniqueNumbers.add(randomNum); 
+function generateRandomNumbers(count, min, max) {
+  // 1: Create a `Set` object
+  let uniqueNumbers = new Set();
+  while (uniqueNumbers.size < count) {
+    // 2: Generate each random number
+    uniqueNumbers.add(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+  // 3: Immediately insert them numbers into the Set...
+  return Array.from(uniqueNumbers);
 }
+// ...set how many numbers to generate from a given range
+console.log(generateRandomNumbers(1026, 1, 1026));
 
-console.log([uniqueNumbers]);
+//link https://www.smashingmagazine.com/2024/08/generating-unique-random-numbers-javascript-using-sets/
+
+
+
+// pokeapi has a total of 1302 records and after number 1024 record(#1025) it starts on a new set of numbers for variations/ forms for record 1025 and on the ID number starts 10001
